5a7613df33244420258727dcc681de7c27b5032a
Xingtao Zeng <2721279z@student.gla.ac.uk>
2023-04-15 18:22:25 +0100

    web test

--- web_test/WebServer.cpp
+++ web_test/WebServer.cpp
@@ -1,50 +1,70 @@
-#include <websocketpp/config/asio_no_tls.hpp>
-#include <websocketpp/server.hpp>
-#include <boost/asio.hpp>
-#include <boost/bind.hpp>
 #include <iostream>
-
-using websocketpp::server;
+#include <string>
+#include <chrono>
+#include <thread>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 int main() {
-    // Create a WebSocket server
-    server<websocketpp::config::asio> ws_server;
-    ws_server.init_asio();
+    // Create a socket for the server to listen on
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        std::cerr << "Failed to create socket" << std::endl;
+        return 1;
+    }
 
-    // Set up the server's callback functions
-    ws_server.set_open_handler([&](websocketpp::connection_hdl hdl) {
-        std::cout << "Client connected" << std::endl;
-    });
+    // Bind the socket to a port
+    sockaddr_in server_address{};
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(12345);
+    if (bind(server_socket, (sockaddr*)&server_address, sizeof(server_address)) == -1) {
+        std::cerr << "Failed to bind socket to port" << std::endl;
+        close(server_socket);
+        return 1;
+    }
 
-    ws_server.set_close_handler([&](websocketpp::connection_hdl hdl) {
-        std::cout << "Client disconnected" << std::endl;
-    });
+    // Listen for incoming connections
+    if (listen(server_socket, SOMAXCONN) == -1) {
+        std::cerr << "Failed to listen on socket" << std::endl;
+        close(server_socket);
+        return 1;
+    }
 
-    // Set up a timer to periodically send updated data to clients
-    boost::asio::deadline_timer timer(ws_server.get_io_service());
-    timer.expires_from_now(boost::posix_time::seconds(1));
-    timer.async_wait(boost::bind([&](const boost::system::error_code& ec) {
-        if (!ec) {
-            // Generate some data to send
-            std::string data = "{\"time\": \"12:00\", \"temperature\": 23.5, \"humidity\": 60, \"light\": 100}";
+    std::cout << "Server is listening on port 12345" << std::endl;
 
-            // Broadcast the data to all connected clients
-            for (auto it = ws_server.get_connections().begin(); it != ws_server.get_connections().end(); ++it) {
-                ws_server.send(*it, data, websocketpp::frame::opcode::text);
-            }
+    // Accept incoming connections
+    sockaddr_in client_address{};
+    socklen_t client_address_size = sizeof(client_address);
+    int client_socket = accept(server_socket, (sockaddr*)&client_address, &client_address_size);
+    if (client_socket == -1) {
+        std::cerr << "Failed to accept incoming connection" << std::endl;
+        close(server_socket);
+        return 1;
+    }
 
-            // Reschedule the timer
-            timer.expires_from_now(boost::posix_time::seconds(1));
-            timer.async_wait(boost::bind([&](const boost::system::error_code& ec) {
-                // Handle timer expiration recursively
-            }));
+    std::cout << "Client connected" << std::endl;
+
+    // Send updated data to the client every second
+    while (true) {
+        std::string data = "{\"time\": \"12:00\", \"temperature\": 23.5, \"humidity\": 60, \"light\": 100}";
+
+        ssize_t bytes_sent = send(client_socket, data.c_str(), data.size(), 0);
+        if (bytes_sent == -1) {
+            std::cerr << "Failed to send data to client" << std::endl;
+            close(client_socket);
+            close(server_socket);
+            return 1;
         }
-    }, _1));
 
-    // Start the server listening on port 12345
-    ws_server.listen(12345);
-    ws_server.start_accept();
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
 
-    // Run the server
-    ws_server.run();
+    // Close the sockets
+    close(client_socket);
+    close(server_socket);
+
+    return 0;
 }
