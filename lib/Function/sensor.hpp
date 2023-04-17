b21bbffb3cd105265a7cee9b3ffe16d7d085ef35
Xingtao Zeng <2721279z@student.gla.ac.uk>
2023-04-17 13:34:26 +0100

    test

--- lib/Function/sensor.hpp
+++ lib/Function/sensor.hpp
@@ -9,6 +9,7 @@
 #include "../Config/DEV_Config.hpp"
 #include <thread>
 #include <mutex>
+#include <condition_variable>
 
 #define MAXTIMINGS  85
 #define DHTPIN    7
@@ -31,6 +32,7 @@ private:
 	void readDHTdataLoop();
 	double lastTemperature = 0.0;
     double lastHumidity = 0.0;
+	std::condition_variable dataCondVar;
 public:
     Sensor(int digitalPin, int dhtPin);
 	~Sensor();
