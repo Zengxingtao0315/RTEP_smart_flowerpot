0a3c03f4af5a1c17c4b3a8f88f1df7c5a1567c8b
Xingtao Zeng <2721279z@student.gla.ac.uk>
2023-04-17 16:32:22 +0100

    test

--- test/main.cpp
+++ test/main.cpp
@@ -156,10 +156,10 @@ int main()
 		temp = Sensor.getTemperature();
         hum = Sensor.getHumidity();
 		std::this_thread::sleep_for(std::chrono::seconds(1));
-		Paint.DrawString_EN(10, 34, "Humidity(%)", &Font16, BLACK, WHITE);
-		Paint.DrawNum(10, 51, hum, &Font16, 1, BLACK, WHITE);
-		Paint.DrawString_EN(10, 68, "Temperature(C)", &Font16, BLACK, WHITE);
-		Paint.DrawNum(10, 85, temp, &Font16, 1, BLACK, WHITE);
+		Paint.DrawString_EN(10, 34, "Humidity(%)", &Font12, BLACK, WHITE);
+		Paint.DrawNum(10, 51, hum, &Font12, 1, BLACK, WHITE);
+		Paint.DrawString_EN(10, 68, "Temperature(C)", &Font12, BLACK, WHITE);
+		Paint.DrawNum(10, 85, temp, &Font12, 1, BLACK, WHITE);
 		
 		//Digital reading of the light emitx`ing diode, 1 for almost no light, 0 for light		
 		digitalValue = Sensor.readDigitalValue();
