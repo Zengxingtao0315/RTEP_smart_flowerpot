14e3f4c6aa177f4a23aa498485edff6af42cef4c
Xingtao Zeng <2721279z@student.gla.ac.uk>
2023-04-17 20:27:16 +0100

    modified bottle

--- Web_Application/html_bottle.py
+++ Web_Application/html_bottle.py
@@ -9,7 +9,7 @@ dhtpin = 23 #BCM is 23, wiring is 4, BORAD is 16
 lightpin = 18 #BCM is 18, wiring is 1, BORAD is 12
 app = Bottle()
 
-@app.route('/')
+@app.route('/test')
 def hello():
     humidity, temperature = Adafruit_DHT.read_retry(11, dhtpin)
     lightFlag = getlightvalue.read_pin(lightpin)
@@ -18,4 +18,4 @@ def hello():
     #str_1 = html_str.html.format(time_now,wendu,shidu)
     return template(web.html, time=time_now,temperature=temperature,humidity=humidity,lightFlag=lightFlag)
 
-run(app, host='192.168.1.103', port=8080) 
+run(app, host='192.168.1.240', port=8080) 
