5dcea71bd57e02dcf1b7ffc187cd113ec75e7ecd
Xingtao Zeng <2721279z@student.gla.ac.uk>
2023-04-17 13:44:23 +0100

    test

--- unit_test/dht_test.cpp
+++ unit_test/dht_test.cpp
@@ -17,7 +17,7 @@ int read_dht11_dat()
  
 	pinMode( DHTPIN, OUTPUT );
 	digitalWrite( DHTPIN, LOW );
-	delay( 20 );
+	delayMicroseconds( 18055 );
 	digitalWrite( DHTPIN, HIGH );
 	delayMicroseconds( 40 );
 	pinMode( DHTPIN, INPUT );
