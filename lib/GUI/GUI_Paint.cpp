01742cdfa799d62f2d6f3f7db515d4c69ee01c0a
Zengxingtao0315 <2721279z@student.gla.ac.uk>
2023-07-23 21:48:29 +0100

    1

--- lib/GUI/GUI_Paint.cpp
+++ lib/GUI/GUI_Paint.cpp
@@ -80,22 +80,6 @@ void Paint::SetScale(UBYTE scale)
     }
 }
 
-/******************************************************************************
-function:	Select Image mirror
-parameter:
-    mirror   :Not mirror,Horizontal mirror,Vertical mirror,Origin mirror
-******************************************************************************/
-void Paint::SetMirroring(UBYTE mirror)
-{
-    if(mirror == MIRROR_NONE || mirror == MIRROR_HORIZONTAL || 
-        mirror == MIRROR_VERTICAL || mirror == MIRROR_ORIGIN) {
-        Debug("mirror image x:%s, y:%s\r\n",(mirror & 0x01)? "mirror":"none", ((mirror >> 1) & 0x01)? "mirror":"none");
-        paint.Mirror = mirror;
-    } else {
-        Debug("mirror should be MIRROR_NONE, MIRROR_HORIZONTAL, \
-        MIRROR_VERTICAL or MIRROR_ORIGIN\r\n");
-    }    
-}
 
 /******************************************************************************
 function: Draw Pixels
