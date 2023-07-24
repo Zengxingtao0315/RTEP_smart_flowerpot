164d663437674b776da89a00603816d6e173dc3d
Zengxingtao0315 <2721279z@student.gla.ac.uk>
2023-07-24 19:19:01 +0100

    1

--- Web_Application/webtestcpp.cpp
+++ Web_Application/webtestcpp.cpp
@@ -4,6 +4,9 @@
 #include <event2/http_struct.h>
 #include <event2/keyvalq_struct.h>
 #include <string>
+#include <thread>  // 添加缺少的头文件
+#include <chrono>  // 添加缺少的头文件
+
 
 // 全局变量，模拟实时更新的数据
 int realtimeData = 0;
