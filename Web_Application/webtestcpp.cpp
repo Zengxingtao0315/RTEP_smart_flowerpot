4d08b7520724eb43b2f276e2f8f527b85731d349
Zengxingtao0315 <2721279z@student.gla.ac.uk>
2023-07-24 18:10:39 +0100

    1

--- Web_Application/webtestcpp.cpp
+++ Web_Application/webtestcpp.cpp
@@ -3,6 +3,7 @@
 #include <websocketpp/server.hpp>
 #include <chrono>
 #include <thread>
+#include <set>
 
 typedef websocketpp::server<websocketpp::config::asio> server;
 
@@ -13,6 +14,19 @@ int main() {
     // 设置监听端口
     int port = 9002;
     ws_server.listen(port);
+
+    // 初始化客户端连接列表
+    std::set<websocketpp::connection_hdl, std::owner_less<websocketpp::connection_hdl>> clients;
+
+    // 设置连接打开事件处理程序
+    ws_server.set_open_handler([&clients](websocketpp::connection_hdl hdl) {
+        clients.insert(hdl);
+    });
+
+    // 设置连接关闭事件处理程序
+    ws_server.set_close_handler([&clients](websocketpp::connection_hdl hdl) {
+        clients.erase(hdl);
+    });
 
     // 启动服务器线程
     ws_server.start_accept();
@@ -30,11 +44,13 @@ int main() {
         // 将数据转换成字符串
         std::string data_str = std::to_string(sensor_data);
 
-        // 广播数据给所有连接的客户端
-        try {
-            ws_server.send_to_all(data_str, websocketpp::frame::opcode::text);
-        } catch (const websocketpp::exception &e) {
-            std::cout << "Error sending data to clients: " << e.what() << std::endl;
+        // 遍历客户端列表，发送数据给所有连接的客户端
+        for (auto it : clients) {
+            try {
+                ws_server.send(it, data_str, websocketpp::frame::opcode::text);
+            } catch (const websocketpp::exception &e) {
+                std::cout << "Error sending data to clients: " << e.what() << std::endl;
+            }
         }
 
         // 等待一段时间，模拟传感器数据的更新频率
