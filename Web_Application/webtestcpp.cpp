a7f41533dcb2142bf0aa9321c2768e12f741a790
Zengxingtao0315 <2721279z@student.gla.ac.uk>
2023-07-24 19:16:51 +0100

    1

--- Web_Application/webtestcpp.cpp
+++ Web_Application/webtestcpp.cpp
@@ -1,81 +1,63 @@
 #include <iostream>
-#include <libwebsockets.h>
+#include <event2/event.h>
+#include <event2/http.h>
+#include <event2/http_struct.h>
+#include <event2/keyvalq_struct.h>
 #include <string>
-#include <thread>
-#include <random>
 
-// WebSocket服务器端口
-#define WEBSOCKET_PORT 8080
+// 全局变量，模拟实时更新的数据
+int realtimeData = 0;
 
-// WebSocket协议
-static const struct lws_protocols protocols[] = {
-    { "example-protocol", callback_example, 0, 0 },
-    { NULL, NULL, 0, 0 } // 结束标记
-};
-
-// WebSocket回调函数
-static int callback_example(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len)
-{
-    switch (reason) {
-        case LWS_CALLBACK_ESTABLISHED: // WebSocket连接建立时触发
-            std::cout << "WebSocket connection established" << std::endl;
-            break;
-
-        case LWS_CALLBACK_CLOSED: // WebSocket连接关闭时触发
-            std::cout << "WebSocket connection closed" << std::endl;
-            break;
-
-        default:
-            break;
+void onRequest(struct evhttp_request* req, void* arg) {
+    struct evbuffer* buf = evbuffer_new();
+    if (!buf) {
+        std::cerr << "Failed to create response buffer." << std::endl;
+        return;
     }
 
-    return 0;
+    // 设置HTTP响应头，指定数据格式为text/event-stream
+    evhttp_add_header(req->output_headers, "Content-Type", "text/event-stream");
+
+    // 构建数据
+    std::string eventData = "data: " + std::to_string(realtimeData) + "\n\n";
+
+    // 将数据写入响应缓冲区
+    evbuffer_add(buf, eventData.c_str(), eventData.size());
+
+    // 发送响应
+    evhttp_send_reply(req, HTTP_OK, "OK", buf);
+
+    // 释放资源
+    evbuffer_free(buf);
 }
 
-// 发送随机数据给客户端
-void sendRandomData(struct lws *wsi)
-{
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<int> dist(1, 100);
-
-    while (lws_service(wsi, 0) >= 0) {
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-
-        int randomData = dist(gen);
-
-        // 将随机数据发送给客户端
-        std::string message = std::to_string(randomData);
-        lws_write(wsi, reinterpret_cast<unsigned char*>(const_cast<char*>(message.c_str())), message.length(), LWS_WRITE_TEXT);
-    }
-}
-
-int main()
-{
-    struct lws_context_creation_info info;
-    memset(&info, 0, sizeof(info));
-
-    info.port = WEBSOCKET_PORT;
-    info.protocols = protocols;
-    info.gid = -1;
-    info.uid = -1;
-
-    struct lws_context *context = lws_create_context(&info);
-
-    if (!context) {
-        std::cerr << "WebSocket server creation failed" << std::endl;
+int main() {
+    struct event_base* base = event_base_new();
+    if (!base) {
+        std::cerr << "Failed to create event base." << std::endl;
         return -1;
     }
 
-    std::thread sendDataThread(sendRandomData, lws_client_connect(context, "localhost", WEBSOCKET_PORT, 0, "/", "localhost", nullptr, nullptr, -1));
-
-    // 进入WebSocket事件循环
-    while (true) {
-        lws_service(context, 50);
+    struct evhttp* http = evhttp_new(base);
+    if (!http) {
+        std::cerr << "Failed to create evhttp." << std::endl;
+        return -1;
     }
 
-    // 清理资源
-    lws_context_destroy(context);
+    evhttp_bind_socket(http, "0.0.0.0", 8080); // 监听地址和端口
 
+    // 设置请求处理回调函数
+    evhttp_set_gencb(http, onRequest, nullptr);
+
+    // 模拟数据更新
+    event_base_loopexit(base, nullptr); // 停止事件循环
+    while (true) {
+        realtimeData++; // 更新数据
+        event_base_loop(base, EVLOOP_NONBLOCK); // 非阻塞模式运行事件循环
+        std::this_thread::sleep_for(std::chrono::seconds(1)); // 1秒更新一次数据
+    }
+
+    evhttp_free(http);
+    event_base_free(base);
     return 0;
 }
