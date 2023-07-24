3b3d17a95b006f9cb34b69710132bbb1b7038e37
Zengxingtao0315 <2721279z@student.gla.ac.uk>
2023-07-24 20:30:11 +0100

    1

--- Web_Application/webtestcpp.cpp
+++ Web_Application/webtestcpp.cpp
@@ -1,82 +1,88 @@
 #include <iostream>
 #include <boost/asio.hpp>
-#include <boost/bind.hpp>
-#include <boost/shared_ptr.hpp>
-#include <boost/make_shared.hpp>
-#include <boost/enable_shared_from_this.hpp>
-class HttpServer : public boost::enable_shared_from_this<HttpServer> {
+#include <boost/bind/bind.hpp> // 新版 Boost 中的 bind 替换
+#include <boost/thread/thread.hpp> // 如果用到线程
+
+using namespace boost::asio;
+using ip::tcp;
+
+class HttpServerSession {
 public:
-    HttpServer(boost::asio::io_service& io_service)
-        : acceptor_(io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 8080)),
-          socket_(io_service) {
-        start_accept();
-    }
+    HttpServerSession(boost::asio::io_service& io_service)
+        : socket_(io_service) {}
 
-private:
-    void start_accept() {
-        acceptor_.async_accept(socket_,
-            boost::bind(&HttpServer::handle_accept, this,
-                        boost::asio::placeholders::error));
-    }
-
-    void handle_accept(const boost::system::error_code& error) {
-        if (!error) {
-            boost::make_shared<HttpServerSession>(boost::ref(socket_))->start();
-        }
-
-        start_accept();
-    }
-
-    boost::asio::ip::tcp::acceptor acceptor_;
-    boost::asio::ip::tcp::socket socket_;
-};
-
-class HttpServerSession : public boost::enable_shared_from_this<HttpServerSession> {
-public:
-    HttpServerSession(boost::asio::ip::tcp::socket socket)
-        : socket_(std::move(socket)) {}
+    tcp::socket& socket() { return socket_; }
 
     void start() {
         boost::asio::async_read_until(socket_, request_, "\r\n\r\n",
-            boost::bind(&HttpServerSession::handle_read, shared_from_this(),
-                        boost::asio::placeholders::error,
-                        boost::asio::placeholders::bytes_transferred));
+            boost::bind(&HttpServerSession::handle_read, this, boost::asio::placeholders::error));
     }
 
 private:
-    void handle_read(const boost::system::error_code& error, std::size_t bytes_transferred) {
+    void handle_read(const boost::system::error_code& error) {
         if (!error) {
             std::istream request_stream(&request_);
-            std::string request_line;
-            std::getline(request_stream, request_line);
+            std::string http_request;
+            std::getline(request_stream, http_request);
+            std::cout << "Received HTTP request: " << http_request << std::endl;
 
-            // 处理请求，并准备响应
             std::string response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
             boost::asio::async_write(socket_, boost::asio::buffer(response),
-                boost::bind(&HttpServerSession::handle_write, shared_from_this(),
-                            boost::asio::placeholders::error));
+                boost::bind(&HttpServerSession::handle_write, this, boost::asio::placeholders::error));
+        }
+        else {
+            delete this;
         }
     }
 
     void handle_write(const boost::system::error_code& error) {
         if (!error) {
-            // 关闭连接
-            boost::system::error_code ec;
-            socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
+            boost::system::error_code ignored_ec;
+            socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both, ignored_ec);
         }
+
+        delete this;
     }
 
-    boost::asio::ip::tcp::socket socket_;
+    tcp::socket socket_;
     boost::asio::streambuf request_;
 };
-int main() {
-    try {
-        boost::asio::io_service io_service;
-        HttpServer server(io_service);
-        io_service.run();
-    } catch (std::exception& e) {
-        std::cerr << "Exception: " << e.what() << "\n";
+
+class HttpServer {
+public:
+    HttpServer(boost::asio::io_service& io_service)
+        : acceptor_(io_service, tcp::endpoint(tcp::v4(), 8080)) {
+        start_accept();
     }
+
+private:
+    void start_accept() {
+        boost::shared_ptr<HttpServerSession> new_session =
+            boost::make_shared<HttpServerSession>(boost::ref(io_service_)); // 使用新版 Boost 的 make_shared
+
+        acceptor_.async_accept(new_session->socket(),
+            boost::bind(&HttpServer::handle_accept, this, new_session, boost::asio::placeholders::error));
+    }
+
+    void handle_accept(boost::shared_ptr<HttpServerSession> session, const boost::system::error_code& error) {
+        if (!error) {
+            session->start();
+        }
+
+        start_accept();
+    }
+
+    boost::asio::io_service& io_service_;
+    tcp::acceptor acceptor_;
+};
+
+int main() {
+    boost::asio::io_service io_service;
+    HttpServer server(io_service);
+
+    boost::thread t(boost::bind(&boost::asio::io_service::run, &io_service)); // 如果用到线程
+
+    io_service.run(); // 或者直接在主线程中运行，如果不用到线程的话
 
     return 0;
 }
