6e31b57d7001b9ec25653cc1b3035ee86f676ba8
Zengxingtao0315 <2721279z@student.gla.ac.uk>
2023-07-30 22:45:08 +0100

    improve comments in GetTime.cpp

--- lib/Function/GetTime.cpp
+++ lib/Function/GetTime.cpp
@@ -8,57 +8,60 @@
 #include "GetTime.hpp"
 
 /*******************************************************************************
-function:
-            Get local time
+Function:
+    Get local time
 *******************************************************************************/
+// Function to get the current local time and return it in the form of a PAINT_TIME struct.
 PAINT_TIME Time::getLocalTime() {
     PAINT_TIME time;
-	std::time_t now;
+    std::time_t now;
     std::tm *local;
 
+    // Get the current time in seconds since the epoch.
     std::time(&now);
+    // Convert the current time to the local time (considering time zone and daylight saving).
     local = std::localtime(&now);
 
-    // Extract the required time information
+    // Extract the required time information (hour, minute, and second) from the local time and store it in the PAINT_TIME struct.
     time.Hour = local->tm_hour;
     time.Min = local->tm_min;
-	time.Sec = local->tm_sec;
-	return time;
+    time.Sec = local->tm_sec;
+    return time;
 }
 
-
-
+// Function to calculate the duration of sunlight in hours based on a digital value (0 or 1).
 float SunlightDurationRecorder::getSunlightDurationInHours(bool digitalValue) {
-	auto now = std::chrono::system_clock::now();
-	if (!initialized_) {
-		// First call to the function to initialize logging information
-		start_time_ = now;
-		duration_0_ = std::chrono::duration<int, std::ratio<1, 1>>(0);
-		last_reset_time_ = now;
-		initialized_ = true;
-	} else {
-		// Check to see if a new day has been reached, and if so reset the duration record
-		auto time_since_last_reset = now - last_reset_time_;
-		if (time_since_last_reset >= std::chrono::hours(24)) {
-			duration_0_ = std::chrono::duration<int, std::ratio<1, 1>>{0};
-			last_reset_time_ = now;
-		}
-	}
+    auto now = std::chrono::system_clock::now();
+    if (!initialized_) {
+        // First call to the function to initialize logging information
+        start_time_ = now;
+        duration_0_ = std::chrono::duration<int, std::ratio<1, 1>>(0);
+        last_reset_time_ = now;
+        initialized_ = true;
+    } else {
+        // Check to see if a new day has been reached, and if so, reset the duration record
+        auto time_since_last_reset = now - last_reset_time_;
+        if (time_since_last_reset >= std::chrono::hours(24)) {
+            duration_0_ = std::chrono::duration<int, std::ratio<1, 1>>{0};
+            last_reset_time_ = now;
+        }
+    }
 
-	// Record duration according to digital
-	auto duration = std::chrono::duration_cast<std::chrono::seconds>(now - start_time_);
-	if (digitalValue == 0) {
-		duration_0_ += duration;
-	} 
-	if (digitalValue == 1) {
+    // Record the duration since the start_time_ according to the digital value (0 or 1).
+    auto duration = std::chrono::duration_cast<std::chrono::seconds>(now - start_time_);
+    if (digitalValue == 0) {
+        duration_0_ += duration;
+    }
+    if (digitalValue == 1) {
+        // If digitalValue is 1, reset the start_time_ to the current time.
         start_time_ = now;
     }
-	
-	int light_duration = duration_0_.count();
-	std::chrono::seconds Drt{light_duration};  // 
+
+    // Get the total duration of sunlight in seconds.
+    int light_duration = duration_0_.count();
+    std::chrono::seconds Drt{light_duration};
+    // Convert the duration to hours and return the result.
     float hours = std::chrono::duration_cast<std::chrono::hours>(Drt).count();
     std::cout << hours << " hours" << std::endl;
-	return hours;
-	
+    return hours;
 }
-
